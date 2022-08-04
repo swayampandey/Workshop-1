53f8c6a26b52c741a8e6e7585354360207076bd9
swayampandey <swayam265@gmail.com>
2022-08-04 12:37:38 +0530

    MODIFIED: [Swayam] Initialized the Board for TicTacToe Game

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -1,9 +1,24 @@
 package com.bridgelabz;
 
 public class TicTacToe {
+	
+	private static char[] board = new char[10];
+	
+	/***
+	 * UC-1 - This method is Assigning method to Board Array
+	 */
+	
+	static void startGame( ) {
+		for(int i = 1; i <=9; i++) {
+			board[i] = ' ';
+		}
+			
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Simulation");
+		
+		startGame();
 
 	}
 
